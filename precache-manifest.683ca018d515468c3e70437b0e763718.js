fecb87c1fa253dba1644b26ebd504e8a6dddf754
adyarcafe <adyarcafe@gmail.com>
2020-07-20 00:03:03 +0530

    Updates

---
+++ precache-manifest.683ca018d515468c3e70437b0e763718.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "525c7132ed2ad612efb6ca6050de27cf",
+    "url": "/cafeadmin.github.io/index.html"
+  },
+  {
+    "revision": "9cd7f04d207bdd511d67",
+    "url": "/cafeadmin.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bc9c86086dcae7151b22",
+    "url": "/cafeadmin.github.io/static/js/2.e30a8bec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cafeadmin.github.io/static/js/2.e30a8bec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cd7f04d207bdd511d67",
+    "url": "/cafeadmin.github.io/static/js/main.2eff4784.chunk.js"
+  },
+  {
+    "revision": "8b8242709e85b8cecf62",
+    "url": "/cafeadmin.github.io/static/js/runtime-main.811dd09f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cafeadmin.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
