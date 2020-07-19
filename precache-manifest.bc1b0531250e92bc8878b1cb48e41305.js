b4cb6bacb53748f13c84214905cbec78fb4cad41
adyarcafe <adyarcafe@gmail.com>
2020-07-20 00:59:47 +0530

    Updates

---
+++ precache-manifest.bc1b0531250e92bc8878b1cb48e41305.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98a598abee05b049b205c29d9a57098e",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "9de703b1d7cb8555ce49",
+    "url": "/react-app/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "bc9c86086dcae7151b22",
+    "url": "/react-app/static/js/2.e30a8bec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.e30a8bec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9de703b1d7cb8555ce49",
+    "url": "/react-app/static/js/main.d60188b0.chunk.js"
+  },
+  {
+    "revision": "1f48e48ab5b9205d612b",
+    "url": "/react-app/static/js/runtime-main.cfe5592d.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-app/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cafeadmin.github.io/precache-manifest.683ca018d515468c3e70437b0e763718.js"
+  "/react-app/precache-manifest.bc1b0531250e92bc8878b1cb48e41305.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cafeadmin.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
